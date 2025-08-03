094a37f3ef2006bb51b892db9cc86455a2e682ed
진호영 <hoyoungjin9148@gmail.com>
2025-08-03 19:32:44 +0900

    Global Style 수정

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -3,10 +3,12 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
+    font-family: 'Noto Sans KR', sans-serif;
   }
 
-  body {
-    font-family: 'Noto Sans KR', sans-serif;
+  body, h1, p, ul, li {
+    margin: 0;
+    paading: 0;
   }
 `;
 

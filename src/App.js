a3bc96748381b107a8a66e4df58a91779f9ce6f4
진호영 <hoyoungjin9148@gmail.com>
2025-08-08 14:56:41 +0900

    AppContainer 조정 및 네이게이션 레이아웃 뼈대 생성

--- src/App.js
+++ src/App.js
@@ -9,24 +9,22 @@ const App = () => {
   return (
     <>
       <GlobalStyle />
-      <AppContainer>
-        <Routes>
-          {/* <Route path="login" element={<Login />} /> */}
-          <Route path="map" element={<Map />} />
-          <Route path="/" element={<MainPage />} />
-        </Routes>
-      </AppContainer>
+      <Routes>
+        {/* <Route path="login" element={<Login />} /> */}
+        <Route path="map" element={<Map />} />
+        <Route path="/" element={<MainPage />} />
+      </Routes>
     </>
   );
 };
 
 export default App;
 
-const AppContainer = styled.div`
-  width: 100%;
-  max-width: 600px;
-  height: 100%;
-  background-color: white;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  padding: 20px;
-`;
+// const AppContainer = styled.div`
+//   width: 100%;
+//   max-width: 600px;
+//   height: 100%;
+//   background-color: white;
+//   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+//   padding: 20px;
+// `;

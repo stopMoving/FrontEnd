b3546e72213d728b06baa18f52bd8f904cdfdef5
진호영 <hoyoungjin9148@gmail.com>
2025-08-10 23:55:11 +0900

    메인 페이지 하단 및 상단 nav바 구현

--- src/components/Layout/MainLayout.jsx
+++ src/components/Layout/MainLayout.jsx
@@ -1,9 +1,47 @@
 import React from "react";
 import { Outlet, Link } from "react-router-dom"; // Link 예시를 위해 추가
 import styled from "styled-components";
+import bookShareIcon from "../../assets/icons/shareIcon";
+
+const bottomNavItems = [
+  { id: 1, iconLabel: "책 나눔하기", icon: "", path: "/" },
+  { id: 2, iconLabel: "책 나눔하기", icon: "", path: "/" },
+  { id: 3, iconLabel: "책 나눔하기", icon: "", path: "/" },
+  { id: 4, iconLabel: "책 나눔하기", icon: "", path: "/" },
+  { id: 5, iconLabel: "책 나눔하기", icon: "", path: "/" },
+];
+
+// 레이아웃 컴포넌트
+const MainLayout = () => {
+  return (
+    <>
+      <TopNav>
+        <div className="logo">🏠</div>
+        <div>
+          <button>로그인</button>
+        </div>
+      </TopNav>
+
+      <AppContainer>
+        {/* 여기에 기능 구현하기 */}
+        <Outlet />
+      </AppContainer>
+
+      <BottomNav>
+        <Link to="/">책 기증</Link>
+        <Link to="/">책 수령</Link>
+        <Link to="/">홈</Link>
+        <Link to="/map">도서관 지도</Link>
+        <Link to="/">AI 추천</Link>
+      </BottomNav>
+    </>
+  );
+};
+
+export default MainLayout;
 
 // 상단 네비게이션 바
-const TopNav = styled.header`
+export const TopNav = styled.header`
   position: fixed;
   top: 0;
   left: 50%;
@@ -30,7 +68,7 @@ const TopNav = styled.header`
 `;
 
 // 하단 네비게이션 바
-const BottomNav = styled.nav`
+export const BottomNav = styled.nav`
   position: fixed;
   bottom: 0;
   left: 50%;
@@ -49,48 +87,18 @@ const BottomNav = styled.nav`
 `;
 
 // 콘텐츠를 담을 컨테이너
-const AppContainer = styled.main`
+export const MainContainer = styled.main`
   width: 100%;
   max-width: 600px;
   min-height: 100%; /* 콘텐츠가 적어도 화면 전체 높이를 차지하도록 */
   background-color: white;
-  /*overflow-y: auto;*/
 
   /* 상단/하단 바 공간 확보를 위한 패딩 */
   /* 상단 바 높이(50px) + 추가 여백(20px) */
-  padding-top: 70px;
+  padding-top: 80px;
   /* 하단 바 높이(60px) + 추가 여백(20px) */
   padding-bottom: 80px;
   /* 좌우 여백 */
   padding-left: 20px;
   padding-right: 20px;
 `;
-
-// 레이아웃 컴포넌트
-const MainLayout = () => {
-  return (
-    <>
-      <TopNav>
-        <div className="logo">🏠</div>
-        <div>
-          <button>로그인</button>
-        </div>
-      </TopNav>
-
-      <AppContainer>
-        {/* 이 자리에 페이지별 콘텐츠가 렌더링됩니다 */}
-        <Outlet />
-      </AppContainer>
-
-      <BottomNav>
-        <Link to="/">책 기증</Link>
-        <Link to="/">책 수령</Link>
-        <Link to="/">홈</Link>
-        <Link to="/map">도서관 지도</Link>
-        <Link to="/">AI 추천</Link>
-      </BottomNav>
-    </>
-  );
-};
-
-export default MainLayout;

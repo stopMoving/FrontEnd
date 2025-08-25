150b22211c19882b6e248e7217aa4ad169a550e6
진호영 <hoyoungjin9148@gmail.com>
2025-08-25 13:45:28 +0900

    refactor: 안쓰는 코드 제거

--- src/App.js
+++ src/App.js
@@ -110,12 +110,3 @@ const App = () => {
 };
 
 export default App;
-
-// const AppContainer = styled.div`
-//   width: 100%;
-//   max-width: 600px;
-//   height: 100%;
-//   background-color: white;
-//   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-//   padding: 20px;
-// `;
